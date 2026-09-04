5e4fdb91392b5de9cad3603980ed66e45cd8199b
Ralph Laude <ralphpaolo.laude@gmail.com~>
2026-09-05 01:07:08 +0800

    Increase animation delay and duration for fade-in effect; enhance navigation link styling.

--- material/overrides/assets/javascripts/home.js
+++ material/overrides/assets/javascripts/home.js
@@ -1,13 +1,13 @@
 fFadeInElements($('.hero'))
 
 function fFadeInElements($container) {
-  var delayStep = 100
+  var delayStep = 150
   let step = 0;
   
   $container.find('*').each(function() {
     $(this)
       .delay(step * delayStep)
-      .animate({ opacity: 1 }, 400);
+      .animate({ opacity: 1 }, 500);
     step++;
   });
 }
